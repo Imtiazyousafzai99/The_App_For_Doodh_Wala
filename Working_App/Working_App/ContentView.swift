40d5f7a9f731c8808d3420efc6a4b146579c5e23
Imtiazyousafzai99 <69912568+Imtiazyousafzai99@users.noreply.github.com>
2026-02-12 01:22:04 +0900

    This is the my first commit

---
+++ Working_App/Working_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Working_App
+//
+//  Created by Imtiaz Alam on 2026/02/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Working_App/Working_App/Working_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Working_AppApp.swift
+//  Working_App
+//
+//  Created by Imtiaz Alam on 2026/02/12.
+//
+
+import SwiftUI
+
+@main
+struct Working_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
